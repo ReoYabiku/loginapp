0ecde89238ffcbc173716711381ae815284cd9be
ReoYabiku <alcremikyu.z@gmail.com>
2022-04-08 15:55:46 +0900

    update root

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "users#top"
-  get 'users/top' => "users#top"
+  get '/' => "users#top"
 end
